174f5a14b08b10f18bec37852765286ee805c195
bumu <bumu@airdb.io>
2023-05-05 18:00:55 +0800

    feat: add token count

--- pkg/monitorkit/monitor.go
+++ pkg/monitorkit/monitor.go
@@ -13,9 +13,26 @@ var GPTRequestCount = promauto.NewCounterVec(
 	[]string{"skey"},
 )
 
+// Total Tokens = Prompt Tokens + Completion Tokens.
 var GTPTokenCont = promauto.NewCounterVec(
 	prometheus.CounterOpts{
 		Name: "chatgw_gtp_token_total",
+		Help: "Total number of gpt requests by skey.",
+	},
+	[]string{"skey"},
+)
+
+var GTPPromptTokens = promauto.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "chatgw_gtp_prompt_tokens_total",
+		Help: "Total number of gpt requests by skey.",
+	},
+	[]string{"skey"},
+)
+
+var GTPCompletionTokens = promauto.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "chatgw_gtp_completion_tokens_total",
 		Help: "Total number of gpt requests by skey.",
 	},
 	[]string{"skey"},
