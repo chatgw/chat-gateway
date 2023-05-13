e8328a7494f02077e3ab7ac829c0991f8c8018a1
卜木 <xqbumu@gmail.com>
2023-05-13 18:16:27 +0800

    chore: add self define table name

--- apps/chatgw/data/schema/key.go
+++ apps/chatgw/data/schema/key.go
@@ -9,3 +9,8 @@ type Key struct {
 	Vendor string // openai azure
 	Token  string
 }
+
+// TableName overrides the table name used by Key to `profiles`
+func (Key) TableName() string {
+	return "tbl_keys"
+}
--- apps/chatgw/data/schema/user.go
+++ apps/chatgw/data/schema/user.go
@@ -8,3 +8,8 @@ type User struct {
 	LoginName string
 	Password  string
 }
+
+// TableName overrides the table name used by User to `profiles`
+func (User) TableName() string {
+	return "tbl_users"
+}
